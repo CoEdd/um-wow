c59dc73bab52b810369f928d791d97c3c88456de
CoEdd <141335632+CoEdd@users.noreply.github.com>
2025-03-01 20:14:00 +0800

    add HomeSection, LaunchCountdown, TourSection, SeniorExperience, and Gallery to index.js

--- umwow/src/pages/index.js
+++ umwow/src/pages/index.js
@@ -1,21 +1,28 @@
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-import AboutSection from '../components/AboutSection';
-import FAQSection from '../components/FAQSection';
-import Chatbot from '../components/Chatbot';
+// pages/index.js
+import React from "react";
+import Navbar from "../components/Navbar";
+import HomeSection from "../components/HomeSection";
+import Footer from "../components/Footer";
+import AboutSection from "../components/AboutSection";
+import FAQSection from "../components/FAQSection";
+import Chatbot from "../components/Chatbot";
+import TourSection from "../components/TourSection";
+import LaunchCountdown from "../components/LaunchCountdown"; // Import the new countdown
+import SeniorExperience from "../components/SeniorExperience"; // Import the new senior experience section
+import Gallery from "../components/Gallery"; // Import the new gallery section
+import "../styles/globals.css";
 
 const Home = () => {
   return (
     <div>
-      <Navbar />
+      {/* <Navbar /> */}
       <main>
-        <section id="home">
-          {/* Home section content here */}
-          <h1>Welcome to UM-WOW!</h1>
-          <p>Everything you need to know as a new student at Universiti Malaya.</p>
-        </section>
-        
+        <HomeSection />
         <AboutSection />
+        <LaunchCountdown />
+        <TourSection />
+        <SeniorExperience />
+        <Gallery /> {/* Add the gallery section here */}
         <FAQSection />
         <Chatbot />
       </main>
